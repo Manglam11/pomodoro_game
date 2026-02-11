0cd87b3b22b2849871f0bbbef3bdb7f60f00fd9b
Manglam <manglamdubey11@gmail.com>
2026-02-11 17:50:08 +0530

    coutdown mechanism implemented

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
+import math
 
-from numpy.f2py.f90mod_rules import fgetdims2
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -11,12 +11,43 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+# def minute_and_sec():
+#     sec = count * 60
+#     minute = count
+def start_timer():
+    global  reps
+    work_sec = 60 * WORK_MIN
+    short_break_sec = 60 * SHORT_BREAK_MIN
+    long_break_sec = 60 * LONG_BREAK_MIN
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+    else:
+        count_down(work_sec)
+
+
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(count):
+    # count *= 60
+    count_mnt = math.floor(count/60)
+    count_sec = count % 60
+    if count_sec in range(0, 10):
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"0{count_mnt}:{count_sec}")
+    print(count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -24,19 +55,24 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+
+
 canvas = Canvas(width= 200, height= 224, bg= YELLOW, highlightthickness= 0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 110, image= tomato_img)
-canvas.create_text(100, 140, text= "00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+timer_text = canvas.create_text(100, 140, text= "00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.grid(row=1, column=1)
+
 
 
 timer_label = Label(text="Timer",bg= YELLOW, highlightthickness=0, fg=GREEN, font=(FONT_NAME, 35, "bold"))
 timer_label.grid(row=0, column=1)
 
+
 chek_box_label = Label(text="✔", bg= YELLOW, highlightthickness=0, fg=GREEN, font=(FONT_NAME, 20, "bold"))
 chek_box_label.grid(row=3, column=1)
-start_button = Button(text="Start" )
+start_button = Button(text="Start", command= start_timer )
 start_button.grid(row=2, column=0)
 finish_button = Button(text="Finish")
 finish_button.grid(row=2, column=2)
